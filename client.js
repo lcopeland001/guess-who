29f486382f214ad62c844b7a314d8ee29ed617bc
Latisha Copeland <lcopeland001@gmail.com>
2022-07-14 21:54:38 -0500

    random person

--- client.js
+++ client.js
@@ -1,15 +1,39 @@
 console.log('Here are all the available people:', people);
 
-$(document).ready(readyNow);
+$(readyNow);
 
 function readyNow(){
     console.log('ready now');
-    addPicture();
+    addPictures();
+    
+    $(document).ready(randomPerson);
 }
 
-function addPicture(){
-    for(let pic of people) $('#picture-content').append(`
-    <div> ${pic.profilePic}</div>
+function addPictures(){
+    console.log('in addPictures');
+    for(let pic of people) {
+    $('#picture-content').append(`
+    <div> 
+    <img src="https://github.com/${pic.githubUsername}.png?size=250" alt="Profile image of ${pic.name}">
+    </div>
     `)
+    }
 }
+
+function randomNumber(min, max){
+    return Math.floor(Math.random() * (1 + max - min) + min);
+}
+
+function randomPerson(){
+    console.log('in randomPerson');
+        for( let i=0; i<people.length; i+=1){
+            let num = randomNumber(0,4);
+            let randomPerson = people[num];
+            $('#random-person').text(`
+            Click on: ${randomPerson.name}
+            `)
+        }
+    }
+console.log('Person:', randomPerson.name);
+
 
--- data.js
+++ data.js
@@ -2,27 +2,27 @@ let people = [
   {
     name: 'Latisha',
     githubUsername: 'lcopeland001',
-    profilePic: '<img src="https://github.com/lcopeland001.png?" size="250" alt="Profile image of Chris"></img>'
+    //profilePic: '<img src="https://github.com/lcopeland001.png?" sizes="250px" alt="Profile image of Chris"></img>'
   },
 
   {
     name: 'John',
     githubUsername: 'Jbaxend1',
-    profilePic: '<img src="https://github.com/Jbaxend1.png?" size="250" alt="Profile image of Chris"></img>'
+    //profilePic: '<img src="https://github.com/Jbaxend1.png?" sizes="250px" alt="Profile image of Chris"></img>'
   },
   {
     name: 'Oscar',
     githubUsername:'oscar-oscar', 
-    profilePic: '<img src="https://github.com/oscar-oscar.png?" size="250" alt="Profile image of Chris"></img>'
+   // profilePic: '<img src="https://github.com/oscar-oscar.png?" sizes="250px" alt="Profile image of Chris"></img>'
   },
   {
     name: 'Alex',
     githubUsername: 'ajsuedkamp',
-    profilePic: '<img src="https://github.com/ajsuedkamp.png?" size="250" alt="Profile image of Chris"></img>'
+    //profilePic: '<img src="https://github.com/ajsuedkamp.png?" sizes="250px" alt="Profile image of Chris"></img>'
   },
   {
     name: 'Kyle',
     githubUsername: '0cticon',
-    profilePic: '<img src="https://github.com/0cticon.png?" size="250" alt="Profile image of Chris"></img>'
+    //profilePic: '<img src="https://github.com/0cticon.png?" size="250px" alt="Profile image of Chris"></img>'
   },
 ];
